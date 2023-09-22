8d25c2e10c12fb08af1275a8bca697d96a1c0847
Konrad <konradnrog@gmail.com>
2023-09-22 20:28:06 +0200

    first commit

---
+++ src/main/java/com/example/gate_mychat_server/GateMychatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gate_mychat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateMychatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateMychatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gate_mychat_server/GateMychatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gate_mychat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateMychatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
