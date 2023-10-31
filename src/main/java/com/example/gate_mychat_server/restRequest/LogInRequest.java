cb80af9d1eb8bd75711e76a52c6c104b8d96ae32
Konrad Groń <konradnrog@gmail.com>
2023-10-31 21:04:49 +0100

    first request created on the gateway

--- src/main/java/com/example/gate_mychat_server/restRequest/LogInRequest.java
+++ src/main/java/com/example/gate_mychat_server/restRequest/LogInRequest.java
@@ -2,32 +2,38 @@ package com.example.gate_mychat_server.restRequest;
 
 import com.example.gate_mychat_server.model.LoginAndPasswordData;
 import com.example.gate_mychat_server.port.out.LogInServicePort;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Service
 public class LogInRequest implements LogInServicePort {
 
 
-    private final String uri = "http://localhost:8082/login";
+    private final URI uri = new URI("http://localhost:8082/authentication/login");
 
+    public LogInRequest() throws URISyntaxException {
+    }
 
 
     @Override
     public Mono<ResponseEntity<String>>  isCorrectCredentials(Mono<LoginAndPasswordData> loginAndPasswordData) {
 
-        Mono<ResponseEntity<String>> result = WebClient.create().post()
+        return WebClient.create().post()
                 .uri(uri)
-             //   .contentType(MediaType.APPLICATION_JSON)
-                .body(loginAndPasswordData, LoginAndPasswordData.class)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromPublisher(loginAndPasswordData, LoginAndPasswordData.class))
                 .retrieve()
                 .toEntity(String.class)
-                .map(responseEntity -> new ResponseEntity<>(responseEntity.getBody(), responseEntity.getHeaders(), responseEntity.getStatusCode()));
-
-        return result;
+                .map(responseEntity -> new ResponseEntity<>(responseEntity.getBody(), responseEntity.getHeaders(), responseEntity.getStatusCode()))
+                .onErrorResume(response -> {
+                  return Mono.just(ResponseEntity.badRequest().body(response.getMessage()));
+                });
     }
 }
